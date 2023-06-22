ff75059da3d9ba9ae76134dbd3057b4187acb104
Jeison pedroso <jeison.contas@gmail.com>
2023-06-21 23:45:38 -0300

    Added categories component

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,22 @@
 import Layout from "../src/components/Layout";
 import Cabecalho from "../src/components/cabecalho";
-
-const Index = (props) => (
-  <Layout>
-    <div>
-      <Cabecalho />
-      <main className="">
-        <div>Testando bg</div>
-
-        <div>Testando bg</div>
-
-        <div>Testando bg</div>
-        <div>Testando bg</div>
-        <div>Testando bg</div>
-      </main>
-      <style jsx>{`
-        li {
-          font-size: ${props.size}rem;
-        }
-      `}</style>
-    </div>
-  </Layout>
-);
+import Categories from "../src/components/categories/categories";
+const Index = (props) => {
+  return (
+    <Layout>
+      <div>
+        <Cabecalho />
+        <main className="">
+          <Categories></Categories>
+        </main>
+        <style jsx>{`
+          li {
+            font-size: ${props.size}rem;
+          }
+        `}</style>
+      </div>
+    </Layout>
+  );
+};
 
 export default Index;
---
+++ src/components/categories/categories.js
@@ -0,0 +1,31 @@
+export default () => {
+  const categories = [
+    "Eletrônicos",
+    "Games e PC Gamer",
+    "Casa e cozinha",
+    "Moda",
+    "Livros,filmes e musica",
+    "Saúde e beleza",
+    "Supermercado",
+    "Esportes e exercicios",
+    "gratis",
+    "Financas",
+    "Familia e criancas",
+    "Delivery",
+    "Viagens",
+    "Carros e motos",
+    "Ferramentas e jardim",
+    "Internet",
+  ];
+  return (
+    <ul className="flex flex-wrap justify-center ">
+      {categories.map((category) => {
+        return (
+          <li className="mr-4 mt-2 p-2 border dark:border-dark-sidebar hover:bg-slate-300 hover:dark:text-light-primary dark:text-dark-text hover:text-dark-primary text-light-text  rounded-lg">
+            <a href="#">{category}</a>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
