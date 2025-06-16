79fb8bbd265f51709954e20a480b7f0a14c25cfb
Jeison pedroso <jeison.contas@gmail.com>
2025-06-16 12:06:21 -0300

    adsense inativado pro enquanto

--- src/components/tabs/AdSenseCard.js
+++ src/components/tabs/AdSenseCard.js
@@ -10,7 +10,7 @@ const AdSenseCard = ({ adClient = "ca-pub-5495811870853736", adSlot, adFormat = 
           // Verificar se o elemento está visível e tem dimensões
           const rect = adRef.current.getBoundingClientRect();
           const isVisible = rect.width > 0 && rect.height > 0;
-          
+
           if (isVisible) {
             console.log('Pushing ad for slot:', adSlot);
             (window.adsbygoogle = window.adsbygoogle || []).push({});
@@ -33,12 +33,13 @@ const AdSenseCard = ({ adClient = "ca-pub-5495811870853736", adSlot, adFormat = 
     return () => clearTimeout(timer);
   }, [adSlot]);
 
+  return (<div></div>)
   return (
     <div className="my-4 p-4 shadow-lg rounded-lg border border-gray-200 bg-white text-center flex flex-col justify-center items-center min-h-[250px] w-full max-w-full">
       <ins
         ref={adRef}
         className="adsbygoogle"
-        style={{ 
+        style={{
           display: 'block',
           width: '100%',
           minHeight: '200px'
